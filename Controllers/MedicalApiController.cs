815e143c622b55848b49bfd1075e848d5a641987
Nabin Soni <nabinkumar570@gmail.com>
2022-09-26 23:57:04 +0530

    Add project files.

---
+++ Controllers/MedicalApiController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MedicalApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MedicalApiController : ControllerBase
+    {
+        //[HttpGet]
+        //public IEnumerable<MedicalApiController> Get()
+        //{
+
+        //}
+    }
+}
